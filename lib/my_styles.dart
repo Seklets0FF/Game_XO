9424eeedf8d02a7f0fcf7330f907cc39f076eacc
sseklecov <s.sekletsov@gmail.com>
2022-10-10 17:48:24 +0300

    Initial commit

---
+++ lib/my_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const TextStyle xoStyle =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0, color: Colors.amber);
+
+const TextStyle txtStyle = TextStyle(
+    fontWeight: FontWeight.bold, fontSize: 30.0, color: Colors.amberAccent);
